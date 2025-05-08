b51c4d7d7f8533024a97be7d1258abbcdc7dbae8
Jayen <emjay3n@Gale.local>
2025-05-08 18:48:19 +0600

    feat: void tab

---
+++ README.md
@@ -0,0 +1,5 @@
+![](docs/images/VoidTab-banner-dark.png#gh-dark-mode-only)
+![](docs/images/VoidTab-banner-light.png#gh-light-mode-only)
+
+A void/blank page for your browser's New Tab page. Tired of annoying items and menus in your home screen? Try VoidTab.
+It literally is a blank page with a solid background. You can customize that background to match your browser theme.
---
+++ popup/popup.js
@@ -0,0 +1,32 @@
+const colorInputFormElement = document.getElementById("color-input-form");
+const darkColorInputElement = document.getElementById("dark-color-input");
+const lightColorInputElement = document.getElementById("light-color-input");
+const resetColorsButtonElement = document.getElementById("reset-colors-button");
+
+// Set page backgrounds from storage values.
+chrome.storage.sync.get(["colors"], (result) => {
+    const colors = result.colors;
+
+    darkColorInputElement.value = colors.darkBackground;
+    lightColorInputElement.value = colors.lightBackground;
+});
+
+// Update storage values from form data.
+colorInputFormElement.onsubmit = (event) => {
+    event.preventDefault();
+
+    const darkBackground = darkColorInputElement.value;
+    const lightBackground = lightColorInputElement.value;
+
+    const colors = {
+        darkBackground,
+        lightBackground,
+    };
+
+    chrome.storage.sync.set({ colors });
+};
+
+// Dispatch `reset-colors` event handled by `background.js`.
+resetColorsButtonElement.onclick = () => {
+    chrome.runtime.sendMessage("reset-colors");
+};
